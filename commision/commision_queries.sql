bf046d1e0b9062b94d7c5f4601253434a0c1eaf4
Uday Nayak <udaynayakgkv@gmail.com>
2020-03-12 15:05:00 +0530

    vtapp

--- commision/commision_queries.sql
+++ commision/commision_queries.sql
@@ -5,18 +5,22 @@ SELECT employees.name,SUM(commission_amount) AS total_commision from employees
 INNER JOIN commissions
 ON commissions.employee_id = employees.id
 GROUP BY employee_id)combined
+ORDER BY total_commision DESC
 LIMIT 1;
 
 SELECT * from employees
 GROUP BY salary
+ORDER BY salary DESC
 LIMIT 3,1;
 
-SELECT departments.name, SUM(commission_amount)from departments
+SELECT * from (
+SELECT departments.id, departments.name, SUM(commission_amount) from departments
 INNER JOIN employees
 ON departments.id = employees.department_id
 INNER JOIN commissions
 ON commissions.employee_id = employees.id
-GROUP BY department_id
+GROUP BY department_id)combined
+ORDER BY `SUM(commission_amount)` DESC
 LIMIT 1;
 
 SELECT * FROM (
