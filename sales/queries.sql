6cc6e5428e803c295edc2877e46c64126377bee1
Uday Nayak <udaynayakgkv@gmail.com>
2020-03-14 17:39:14 +0530

    vtapp

--- sales/queries.sql
+++ sales/queries.sql
@@ -1,5 +1,6 @@
 use sales;
 
+/* 1 */
 SELECT salesperson.id, salesperson.name, customer.name AS `customer name` from salesperson
 INNER JOIN orders
 ON orders.salesperson_id = salesperson.id
@@ -7,12 +8,14 @@ INNER JOIN customer
 ON orders.cust_id = customer.id
 WHERE customer.name = 'Samsonic';
 
+/* 2 */
 SELECT *, COUNT(orders.id) AS `order count` from  salesperson
 INNER JOIN orders
 ON orders.salesperson_id = salesperson.id
 GROUP BY salesperson.id
 HAVING `order count` >=  2;
 
+/* 3 */
 SELECT * from(
   SELECT salesperson.*, orders.order_date from salesperson
 INNER JOIN orders
@@ -22,6 +25,7 @@ ORDER BY orders.order_date DESC)combined
 GROUP BY id
 ORDER BY id;
 
+/* 4 */
 SELECT SUM(orders.amount) AS `total orders amount`from salesperson
 INNER JOIN orders
 on orders.salesperson_id = salesperson.id
@@ -36,6 +40,7 @@ on orders.salesperson_id = salesperson.id
 GROUP BY salesperson.id
 HAVING `total orders amount` = @max_amount;
 
+/* 5 */
 SELECT salesperson.name, GROUP_CONCAT(customer.industry_type) from salesperson
 INNER JOIN orders
 ON orders.salesperson_id = salesperson.id
@@ -43,6 +48,7 @@ INNER JOIN customer
 ON orders.cust_id = customer.id
 GROUP BY salesperson.id;
 
+/* 6 */
 SELECT salesperson.name, SUM(orders.amount) AS `sales amount` from salesperson
 INNER JOIN orders
 ON orders.salesperson_id = salesperson.id
