f943e1721237cb06e034565c7623e406c0f6b6f9
Uday Nayak <udaynayakgkv@gmail.com>
2020-03-16 11:24:43 +0530

    vtapp

--- commision/commision_queries.sql
+++ commision/commision_queries.sql
@@ -1,31 +1,28 @@
 use commission;
 
-SELECT * FROM (
 SELECT employees.name,SUM(commission_amount) AS total_commision from employees
 INNER JOIN commissions
 ON commissions.employee_id = employees.id
-GROUP BY employee_id)combined
+GROUP BY employee_id
 ORDER BY total_commision DESC
 LIMIT 1;
 
-SELECT * from employees
-GROUP BY salary
+SELECT name, salary from employees
+GROUP BY id
 ORDER BY salary DESC
 LIMIT 3,1;
 
-SELECT * from (
 SELECT departments.id, departments.name, SUM(commission_amount) from departments
 INNER JOIN employees
 ON departments.id = employees.department_id
 INNER JOIN commissions
 ON commissions.employee_id = employees.id
-GROUP BY department_id)combined
+GROUP BY department_id
 ORDER BY `SUM(commission_amount)` DESC
 LIMIT 1;
 
-SELECT * FROM (
 SELECT employees.name,SUM(commission_amount) AS total_commision from employees
 INNER JOIN commissions
 ON commissions.employee_id = employees.id
-GROUP BY employee_id)combined
-WHERE total_commision > 4000;
+GROUP BY employee_id
+HAVING total_commision > 4000;
