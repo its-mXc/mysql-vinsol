c781981a2f4f03ebc86e26a6beb976138f02c1a2
Uday Nayak <udaynayakgkv@gmail.com>
2020-03-18 12:49:18 +0530

    vtapp

--- vtapp/dump.sql
+++ vtapp/dump.sql
@@ -23,16 +23,20 @@ DROP TABLE IF EXISTS `assigned`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assigned` (
+  `id` varchar(2) PRIMARY KEY,
   `task_id` varchar(2) NOT NULL,
   `runner_id` varchar(2) NOT NULL,
   `reviwer_id` varchar(2) NOT NULL,
   `status` varchar(2) DEFAULT '1',
+  `link_id` varchar(2) DEFAULT NULL,
   KEY `task_id` (`task_id`),
   KEY `runner_id` (`runner_id`),
   KEY `reviwer_id` (`reviwer_id`),
+  KEY `link_id` (`link_id`),
   CONSTRAINT `assigned_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`),
   CONSTRAINT `assigned_ibfk_2` FOREIGN KEY (`runner_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `assigned_ibfk_3` FOREIGN KEY (`reviwer_id`) REFERENCES `user` (`id`)
+  CONSTRAINT `assigned_ibfk_3` FOREIGN KEY (`reviwer_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `assigned_ibfk_4` FOREIGN KEY (`link_id`) REFERENCES `link` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -43,6 +47,170 @@ CREATE TABLE `assigned` (
 LOCK TABLES `assigned` WRITE;
 /*!40000 ALTER TABLE `assigned` DISABLE KEYS */;
 /*!40000 ALTER TABLE `assigned` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `assigned_exercises`
+--
+
+DROP TABLE IF EXISTS `assigned_exercises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assigned_exercises` (
+  `id` varchar(2) NOT NULL,
+  `exercise_id` varchar(2) DEFAULT NULL,
+  `runner_id` varchar(2) DEFAULT NULL,
+  `status` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `exercise_id` (`exercise_id`),
+  KEY `runner_id` (`runner_id`),
+  CONSTRAINT `assigned_exercises_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (`id`),
+  CONSTRAINT `assigned_exercises_ibfk_2` FOREIGN KEY (`runner_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assigned_exercises`
+--
+
+LOCK TABLES `assigned_exercises` WRITE;
+/*!40000 ALTER TABLE `assigned_exercises` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assigned_exercises` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `assigned_track`
+--
+
+DROP TABLE IF EXISTS `assigned_track`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assigned_track` (
+  `track_id` varchar(2) DEFAULT NULL,
+  `runner_id` varchar(2) DEFAULT NULL,
+  `reviwer_id` varchar(2) DEFAULT NULL,
+  KEY `track_id` (`track_id`),
+  KEY `runner_id` (`runner_id`),
+  KEY `reviwer_id` (`reviwer_id`),
+  CONSTRAINT `assigned_track_ibfk_1` FOREIGN KEY (`track_id`) REFERENCES `track` (`id`),
+  CONSTRAINT `assigned_track_ibfk_2` FOREIGN KEY (`runner_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `assigned_track_ibfk_3` FOREIGN KEY (`reviwer_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assigned_track`
+--
+
+LOCK TABLES `assigned_track` WRITE;
+/*!40000 ALTER TABLE `assigned_track` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assigned_track` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` varchar(2) NOT NULL,
+  `content` varchar(100) DEFAULT NULL,
+  `user_id` varchar(2) DEFAULT NULL,
+  `assigned_id` varchar(2) DEFAULT NULL,
+  `timestamp` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `assigned_id` (`assigned_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`assigned_id`) REFERENCES `assigned` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exercise`
+--
+
+DROP TABLE IF EXISTS `exercise`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exercise` (
+  `id` varchar(2) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `track_id` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `track_id` (`track_id`),
+  CONSTRAINT `exercise_ibfk_1` FOREIGN KEY (`track_id`) REFERENCES `track` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exercise`
+--
+
+LOCK TABLES `exercise` WRITE;
+/*!40000 ALTER TABLE `exercise` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exercise` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `link`
+--
+
+DROP TABLE IF EXISTS `link`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `link` (
+  `id` varchar(2) NOT NULL,
+  `href` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `link`
+--
+
+LOCK TABLES `link` WRITE;
+/*!40000 ALTER TABLE `link` DISABLE KEYS */;
+/*!40000 ALTER TABLE `link` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `owner`
+--
+
+DROP TABLE IF EXISTS `owner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `owner` (
+  `track_id` varchar(2) DEFAULT NULL,
+  `owner_id` varchar(2) DEFAULT NULL,
+  KEY `track_id` (`track_id`),
+  KEY `owner_id` (`owner_id`),
+  CONSTRAINT `owner_ibfk_1` FOREIGN KEY (`track_id`) REFERENCES `track` (`id`),
+  CONSTRAINT `owner_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `owner`
+--
+
+LOCK TABLES `owner` WRITE;
+/*!40000 ALTER TABLE `owner` DISABLE KEYS */;
+/*!40000 ALTER TABLE `owner` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -130,4 +298,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-16 11:45:56
+-- Dump completed on 2020-03-18 12:45:27
